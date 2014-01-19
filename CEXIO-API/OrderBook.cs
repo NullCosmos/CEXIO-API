fd74ada562a715ec68a72a7f7eddacc19d142e02
Andrew Meyer <andrew@gamingvibe.com>
2014-01-18 19:12:18 -0500

    Cleaned up usings

--- CEXIO-API/OrderBook.cs
+++ CEXIO-API/OrderBook.cs
@@ -1,12 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
-using System.Collections.Specialized;
 using Newtonsoft.Json;
-using System.IO;
 
 namespace CEXIO_API
 {
@@ -19,21 +14,10 @@ namespace CEXIO_API
         public List<List<Double>> bids = new List<List<Double>>();
         public List<List<Double>> asks = new List<List<Double>>();
 
-        //public class Order
-        //{
-        //    public Double price = 0;
-        //    public Double amount = 0;
-        //}
-
         public static OrderBook Deserialize(String json)
         {
             return JsonConvert.DeserializeObject<OrderBook>(json);
         }
-
-        //public static List<Order> DeserializeList(String json)
-        //{
-        //    return JsonConvert.DeserializeObject<List<Order>>(json);
-        //}
 
         public static OrderBook GetOrderBook(CEXIO.Commodity commodity)
         {
